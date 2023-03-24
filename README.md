af8f7093740c0af5f4fb5b22b3aa16435ce3eec1
Shahriar <yazdipour@users.noreply.github.com>
2023-03-24 14:23:57 +0100

    Init Openapi code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # text-to-sql-gpt
+
+```bash
+pip install -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,35 @@
+import openai
+
+with open('.env') as f:
+    env = dict(line.strip().split('=') for line in f)
+
+openai.organization = env['OPENAI_ORGANIZATION']
+openai.api_key = env['OPENAI_API_KEY']
+model_name = "gpt-3.5-turbo"
+
+message_history = []
+
+
+def chat(inp, role="user"):
+    message_history.append({"role": role, "content": f"{inp}"})
+    completion = openai.ChatCompletion.create(
+        model=model_name,
+        messages=message_history
+    )
+    reply_content = completion.choices[0].message.content
+    message_history.append(
+        {"role": "assistant", "content": f"{reply_content}"})
+    return reply_content
+
+
+# calculate the cost: "gpt-3.5-turbo" is "ChatGPT" $0.002 per 1k tokens
+def cost(reply_content):
+    tokens = len(reply_content)
+    cost = tokens / 1000 * 0.002
+    return tokens, cost
+
+
+res = chat("Hello, how are you?")
+print(res)
+tokens, cost = cost(res)
+print(f"tokens: {tokens}, cost: {cost}$")
