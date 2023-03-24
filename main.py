090d65390fbf2e0f843befbb624ddbf1ea410fc9
Shahriar <yazdipour@users.noreply.github.com>
2023-03-24 15:01:44 +0100

    Rm org api key

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import openai
 with open('.env') as f:
     env = dict(line.strip().split('=') for line in f)
 
-openai.organization = env['OPENAI_ORGANIZATION']
 openai.api_key = env['OPENAI_API_KEY']
 model_name = "gpt-3.5-turbo"
 
