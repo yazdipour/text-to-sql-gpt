f8ed9a5ad8c99c56c943e91f13427a88c4102f50
Shahriar <yazdipour@users.noreply.github.com>
2023-05-08 13:43:58 +0200

    try improve t5 eval

--- src/eval/eval_experiment.bat
+++ src/eval/eval_experiment.bat
@@ -3,22 +3,22 @@
 rem Define SEOSSPATH variable
 set "SEOSSPATH=../../data/seoss/"
 
-rem Define UNBALANCED directory
-set "UNBALANCED=balanced/"
+rem Define subdir variable
+set "SUBDIR=balanced/"
 
 @REM TODO: Only change here for evaluation
 rem Define GOLDNAME variable
-for %%i in ("%SEOSSPATH%gold_specific.sql") do set "GOLDNAME=%%~ni"
+for %%i in ("%SEOSSPATH%gold.sql") do set "GOLDNAME=%%~ni"
 
 rem Define MODEL variable
 set "MODEL=gpt-3.5-turbo"
 
-rem Define other variables using SEOSSPATH, GOLDNAME, MODEL, and UNBALANCED
-set "gold=%SEOSSPATH%%UNBALANCED%%GOLDNAME%.sql"
-set "pred=%SEOSSPATH%%UNBALANCED%predictions-%MODEL%-%GOLDNAME%.sql"
+rem Define other variables using SEOSSPATH, GOLDNAME, MODEL, and SUBDIR
+set "gold=%SEOSSPATH%%SUBDIR%%GOLDNAME%.sql"
+set "pred=%SEOSSPATH%%SUBDIR%predictions-%MODEL%-%GOLDNAME%.sql"
 set "db=%SEOSSPATH%database"
 set "table=%SEOSSPATH%tables.json"
-set "eval_output=%SEOSSPATH%%UNBALANCED%eval_breakdown-%MODEL%-%GOLDNAME%.txt"
+set "eval_output=%SEOSSPATH%%SUBDIR%eval_breakdown-%MODEL%-%GOLDNAME%.txt"
 
 rem Execute the command with the extracted paths
 python evaluation.py --gold "%gold%" --pred "%pred%" --db "%db%" --table "%table%" > "%eval_output%" --etype all
